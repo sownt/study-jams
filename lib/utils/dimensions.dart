71573ec71d8198dd12e8ecd09096ff2d76cbac4c
Son Tran Thai <thonsai@outlook.com>
2023-07-26 21:23:25 +0700

    add dimens config

---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,11 @@
+class Dimensions {
+  Dimensions._();
+  static const xs = 4.0;
+  static const s = 8.0;
+  static const sm = 12.0;
+  static const m = 16.0;
+  static const sl = 24.0;
+  static const l = 32.0;
+  static const xl = 36.0;
+  static const xxl = 48.0;
+}
